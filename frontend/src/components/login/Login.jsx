4176468284a755287e992ea7b28ad64d8506ca8b
AlexEmrik <alexander.karolin@hotmail.com>
2023-09-11 14:17:00 +0200

    small changes to login

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -7,6 +7,7 @@ function Login() {
       <div class="login__container">
          <div class="login__containerLeft">
               <h1>Logga in</h1>
+              <p className='login__textLeft'>Se till att logga in idag och boka din tvättid!</p>
               <input type="email" placeholder="Email"/>
               <input type="password" placeholder="Lösenord"/><br/>
               <button className='login__buttonLeft'>Logga in</button>
@@ -14,8 +15,8 @@ function Login() {
       </div>
       <div class="login__register">
           <div class="login__containerRight">
-              <h2>Hallå granne!</h2>
-              <p>Se till att komma igång och boka tvättider idag</p>
+              <h2 className='login__textRight'>Hallå granne!</h2>
+              <p className='login__textRight'>Om du inte redan har ett BRF konto, gör ett idag!</p>
               <input type="text" placeholder='Förnamn' />
               <input type="text" placeholder='Efternamn' />
               <input type="email" placeholder="Email"/>
